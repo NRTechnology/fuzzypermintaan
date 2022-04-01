d89155e72cfdddac14acb21938f41db655cc8c45
LENOVO <nrtechnologyid@gmail.com>
2022-04-01 17:05:58 +0700

    final

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# This is a sample Python script.
+def fuzzy_union(nilai_a, nilai_b):
+    if nilai_a < nilai_b:
+        return nilai_a
+    return nilai_b
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     permintaan = {
         'min': 1000,
@@ -35,23 +35,61 @@ if __name__ == '__main__':
     # algo permintaan turun
     # (max permintaan-permintaan) bagi (max permintaan - min permintaan)
     permintaan_turun = (permintaan['max'] - kasus['permintaan']) / (permintaan['max'] - permintaan['min'])
-    print(permintaan_turun)
 
     # algo permintaan Naik
     # (permintaan-min permintaan) bagi (max permintaan - min permintaan)
     permintaan_naik = (kasus['permintaan'] - permintaan['min']) / (permintaan['max'] - permintaan['min'])
-    print(permintaan_naik)
 
-    # algo persediaan turun
+    # algo persediaan sedikit
     # (max persediaan-persediaan) bagi (max persediaan - min persediaan)
-    persediaan_turun = (persediaan['max'] - kasus['persediaan']) / (persediaan['max'] - persediaan['min'])
-    print(persediaan_turun)
+    persediaan_sedikit = (persediaan['max'] - kasus['persediaan']) / (persediaan['max'] - persediaan['min'])
 
-    # algo persediaan Naik
+    # algo persediaan banyak
     # (persediaan-min persediaan) bagi (max persediaan - min persediaan)
-    persediaan_naik = (kasus['persediaan'] - persediaan['min']) / (persediaan['max'] - persediaan['min'])
-    print(persediaan_naik)
+    persediaan_banyak = (kasus['persediaan'] - persediaan['min']) / (persediaan['max'] - persediaan['min'])
 
+    alpred_z = []
+    # rule 1
+    apred1 = fuzzy_union(permintaan_turun, persediaan_banyak)
+    z1 = produksi['max'] - (apred1 * (produksi['max'] - produksi['min']))
+    alpred_z.append({
+        'apred': apred1,
+        'z': z1
+    })
 
+    # rule 2
+    apred2 = fuzzy_union(permintaan_turun, persediaan_sedikit)
+    z2 = produksi['max'] - (apred2 * (produksi['max'] - produksi['min']))
+    alpred_z.append({
+        'apred': apred2,
+        'z': z2
+    })
+
+    # rule 3
+    apred3 = fuzzy_union(permintaan_naik, persediaan_banyak)
+    z3 = produksi['min'] + (apred3 * (produksi['max'] - produksi['min']))
+    alpred_z.append({
+        'apred': apred3,
+        'z': z3
+    })
+
+    # rule 3
+    apred4 = fuzzy_union(permintaan_naik, persediaan_sedikit)
+    z4 = produksi['min'] + (apred4 * (produksi['max'] - produksi['min']))
+    alpred_z.append({
+        'apred': apred4,
+        'z': z4
+    })
+
+    pembilang_rerata = 0
+    for data in alpred_z:
+        pembilang_rerata += (data['apred'] * data['z'])
+
+    penyebut_rerata = 0
+    for data in alpred_z:
+        penyebut_rerata += data['apred']
+
+    rerata = pembilang_rerata / penyebut_rerata
+    print("Produksi " + str(int(rerata)))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
